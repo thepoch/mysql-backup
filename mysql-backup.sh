0f9887ea298ad6d3e868fac9473194a4cca3e65f
thepoch <25760806+thepoch@users.noreply.github.com>
2021-01-06 19:24:34 +0800

    Create mysql-backup.sh

---
+++ mysql-backup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Script that uses mysqldump to backup MySQL databases.
+# NOTE: This requires a user with the following minimum privileges
+# for backing up:
+#  - SELECT
+#  - LOCK TABLES
+#  - SHOW VIEW (if you use views)
+#  - EVENT (if you use events)
+
+# Configuration file that contains USERNAME, PASSWORD, BACKUPDIR,
+# DBLIST, and DBLIST_NODATE.
+CONFIG=`dirname $0`/mysql-backup.conf
+if [ -f $CONFIG ]; then
+	. $CONFIG
+else
+	echo "ERROR: Configuration file not found."
+	exit 1
+fi
+
+# Today in YYYYMMDD format.
+TODAY=`date +%Y-%m-%d-%H-%M-%S`
+
+# Create the BACKUPDIR if it doesn't exist.
+if [ ! -d "$BACKUPDIR" ]; then
+	mkdir -pv "$BACKUPDIR"
+fi
+
+# Let's go to the BACKUPDIR.
+cd $BACKUPDIR
+
+# Backup process - DATE
+#
+for DATABASE in $DBLIST; do
+	mysqldump -h $HOSTNAME -u $USERNAME --password=$PASSWORD --routines --events $DATABASE > $DATABASE-$TODAY.sql
+	gzip $DATABASE-$TODAY.tar.gz $DATABASE-$TODAY.sql
+	rm $DATABASE-$TODAY.sql
+done
+
+# Backup process - NODATE
+#
+for DATABASE in $DBLIST_NODATE; do
+	if [ -f $DATABASE-current.tar.gz ]; then
+		mv $DATABASE-current.tar.gz $DATABASE-previous.tar.gz
+	fi
+	mysqldump -h $HOSTNAME -u $USERNAME --password=$PASSWORD --routines --events $DATABASE > $DATABASE-$TODAY.sql
+	gzip $DATABASE-current.tar.gz $DATABASE-$TODAY.sql
+	rm $DATABASE-$TODAY.sql
+done
